7f734543af477e982d6343c3120e855eddcdc200
Agbajeolayiwola <agbajeolaiwola@gmail.com>
2023-12-20 16:27:55 +0100

    first commit

--- components/SVGs/CancelSvg.tsx
+++ components/SVGs/CancelSvg.tsx
@@ -1,4 +1,7 @@
-const CancelSvg = () => {
+interface CancelSvgProps {
+  onClick: () => void
+}
+const CancelSvg: React.FC<CancelSvgProps> = ({ onClick }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
